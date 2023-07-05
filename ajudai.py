e62d0617f3bc15c93a725c276a24647fe3cf732f
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2023-07-05 11:02:12 -0300

    Sub-programas relacionados à conexão com a API OpenAI foram agrupados numa classe estática; tais sub-programas tiveram suas assinaturas modificadas, e estruturas foram repartidas noutros sub-programas, quando apropriado.

--- ajudai.py
+++ ajudai.py
@@ -1,8 +1,75 @@
 # AjudaAI - Assistente virtual para sistemas operacionais baseados em Unix.
 import openai, argparse, os
 
-# :/
-ENV_KEY_NAME = "OPENAI_API_KEY"
+
+class OAPI():
+    _ENV_API_KEY_NAME = "OPENAI_API_KEY"
+
+
+    # Procedimento para definir chave da API, se for válida.
+    def set_api_key(key: str):
+        if not OAPI.is_set_api_key():
+            if OAPI.is_valid_api_key(key):
+                os.putenv(self._ENV_API_KEY_NAME, key)
+            else:
+                print("Chave inválida.")
+        else:
+            if OAPI.is_valid_api_key(os.getenv(self._ENV_API_KEY_NAME)):
+                print("Uma sessão já está ativa.")
+            else:
+                print("Uma sessão inválida está ativa, e será desativada.")
+                OAPI.unset_api_key()
+
+
+    # Procedimento para desativar chave da API.
+    def unset_api_key(anything):
+        if OAPI.is_set_api_key():
+            os.unsetenv(_ENV_API_KEY_NAME)
+            print("Desconectado.")
+        else:
+            print("Nenhuma sessão ativa.")
+
+
+    # Função para verificar se uma chave está definida.
+    def is_set_api_key():
+        if OAPI._ENV_API_KEY_NAME in os.environ.keys():
+            return True
+        else:
+            return False
+
+
+    # Função para verificar se uma chave é válida.
+    def is_valid_api_key(key: str):
+        try:
+            openai.api_key = os.getenv(self._ENV_API_KEY_NAME)
+            openai.Completion.create(
+                model="gpt-3.5-turbo",
+                prompt="Say this is a test",
+                max_tokens=7,
+                temperature=0
+            )
+        except Exception as e:
+            if type(e) == openai.error.AuthenticationError:
+                return False
+        else:
+            return True
+
+
+    def submit(self, request: str, explain: bool = False):
+        prompt= self.build_prompt(request, self.os_name, self.shell_name, explain=explain)
+
+        reponse = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = [
+                {"role": "system", "content": "Você é uma aplicação CLI que gera comandos de terminal"},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens = 300 if explain else 180,
+            temperature = 0
+        )
+
+        return reponse["choices"][0]["message"]["content"]
+
 
 # Função para avaliar argumentos passados ao programa e retornar resultados.
 def parse_cli_args():
@@ -23,8 +90,8 @@ def parse_cli_args():
     subparsers.add_parser("comando", aliases=["c"])
 
     subparsers.choices["conectar"].add_argument("CHAVE")
-    subparsers.choices["conectar"].set_defaults(func=login)
-    subparsers.choices["desconectar"].set_defaults(func=logout)
+    subparsers.choices["conectar"].set_defaults(func=OAPI.set_api_key)
+    subparsers.choices["desconectar"].set_defaults(func=OAPI.unset_api_key)
     subparsers.choices["pergunta"].add_argument("PERGUNTA")
     #subparsers.choices["pergunta"].set_defaults()
     subparsers.choices["comando"].add_argument("COMANDO")
@@ -33,94 +100,40 @@ def parse_cli_args():
     return parser.parse_args()
 
 
-# Controlador responsável por articular as respostas da API com o sistema
-# operacional.
-class Controller:
-    def login():
-        print("Hello")
-
-# Classe que outorga a interação com a OpenAI API.
-class OAPI:
-    def __init__(self, distro_name: str, user: str, api_key: str):
-        self.distro_name = distro_name
-        self.user = user
-        openai.api_key = api_key
-
-
-    # 'explain' é um parâmetro opcional para determinar se uma explicação será
-    # fornecida ao usuário.
-    def build_prompt(self, distro_name, request: str, explain: bool = False):
-        explain_text = ""
-        format_text = "Comando: <inserir comando aqui>"
-
-        if explain:
-            explain_text = ("Também quero uma explicação detalhada sobre o"
-                "código e como ele funciona")
-            format_text += ("\nDescrição: <inserir descrição aqui>\nA descrição"
-                            "deve ser escrita na mesma língua que a pergunta")
-        
-        # Lista que contém todas as informações que criam o prompt ideal
-        #prompt_list = [ 
-        #    (f"Instruções: Escreva um comando CLI que faz o seguinte: {request}.
-        #               Tenha certeza de que esse comando está correto e funciona
-        #               no {distro_name}. {explain_text}"),
-        #    (f"Formato: {format_text}"),
-        #    (f"Caso a explicação não seja pedida de forma explícita mostre apenas
-        #               o comando e nenhuma explicação acerca do comando"),
-        #    (f"Mostre o comando sem o caractere ` ou ´ ou '"),
-        #    (f"Certifique de usar o formato acima de forma exata"),
-        #]
-
-        # Retorna uma string contendo as informações do prompt_list separadas por \n\n
-        return "\n\n".join(prompt_list)
-
-
-    def ask(self, request: str, explain: bool = False):
-        prompt= self.build_prompt(request, self.os_name, self.shell_name, explain=explain)
-
-        openai.api_key = "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"
-
-        reponse = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            messages = [
-                {"role": "system", "content": "Você é uma aplicação CLI que gera comandos de terminal"},
-                {"role": "user", "content": prompt}
-            ],
-            max_tokens = 300 if explain else 180,
-            temperature = 0
-        )
-
-        return reponse["choices"][0]["message"]["content"]
-
-    
-def login(key: str):
-    if "OPENAI_API_KEY" in os.environ.keys():
-        print("Você tem uma sessão ativa.\nPara desativá-la, use o sub-comando"
-              "'desconectar'.")
-    else:
-        # Teste de validade da chave.
-        try:
-            openai.api_key = key
-            openai.Completion.create(
-                model="gpt-3.5-turbo",
-                prompt="Say this is a test",
-                max_tokens=7,
-                temperature=0
-            )
-        except Exception as e:
-            if type(e) == openai.error.AuthenticationError:
-                print("Chave inválida.")
-        else:
-            os.putenv("OPENAI_API_KEY", key)
-            print("Conexão exitosa.")
-
-
-def logout(anything):
-    if ENV_KEY_NAME in os.environ.keys():
-        os.unsetenv(ENV_KEY_NAME)
-        print("Desconectado")
-    else:
-        print("Nenhuma sessão ativa.")
+## Classe que outorga a interação com a OpenAI API.
+#class OAPI:
+#    def __init__(self, distro_name: str, user: str, api_key: str):
+#        self.distro_name = distro_name
+#        self.user = user
+#        openai.api_key = api_key
+#
+#
+#    # 'explain' é um parâmetro opcional para determinar se uma explicação será
+#    # fornecida ao usuário.
+#    def build_prompt(self, distro_name, request: str, explain: bool = False):
+#        explain_text = ""
+#        format_text = "Comando: <inserir comando aqui>"
+#
+#        if explain:
+#            explain_text = ("Também quero uma explicação detalhada sobre o"
+#                "código e como ele funciona")
+#            format_text += ("\nDescrição: <inserir descrição aqui>\nA descrição"
+#                            "deve ser escrita na mesma língua que a pergunta")
+#        
+#        # Lista que contém todas as informações que criam o prompt ideal
+#        #prompt_list = [ 
+#        #    (f"Instruções: Escreva um comando CLI que faz o seguinte: {request}.
+#        #               Tenha certeza de que esse comando está correto e funciona
+#        #               no {distro_name}. {explain_text}"),
+#        #    (f"Formato: {format_text}"),
+#        #    (f"Caso a explicação não seja pedida de forma explícita mostre apenas
+#        #               o comando e nenhuma explicação acerca do comando"),
+#        #    (f"Mostre o comando sem o caractere ` ou ´ ou '"),
+#        #    (f"Certifique de usar o formato acima de forma exata"),
+#        #]
+#
+#        # Retorna uma string contendo as informações do prompt_list separadas por \n\n
+#        return "\n\n".join(prompt_list)
 
 
 if __name__ == "__main__":
